b5fc6d8a53129bd47e25caface4c73ace12cbf61
Dojinex <bitrusdauda9118@gmail.com.com>
2022-09-20 08:14:51 -0700

    task 6-print_comb3.py commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if i % 10 != i / 10 and i % 10 > i / 10:
-        print("{:02d}".format(i), end='')
-        if (i / 10 < 8):
-            print(", ", end="")
-            print()
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
