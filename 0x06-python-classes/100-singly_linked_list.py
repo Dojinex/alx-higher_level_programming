832261bf5c473cc0be2f9acd1c00708d4edca066
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-12 02:54:26 -0700

    task 100 commit

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,70 +1,74 @@
 #!/usr/bin/python3
-"""module for a singly linked list"""
+"""Singly linked list"""
+
 
 class Node:
-    """"defines a node"""
+    """Representation of a list node"""
+
     def __init__(self, data, next_node=None):
-        """initializes the node with instance variables"""
+        """Instantiation with data and next_node"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """gets data attribute"""
-        return (self.__data)
+        """Property to retrieve data"""
+        return self.__data
 
     @data.setter
-    def data(self, value):
-        """sets data attribute"""
-        if not isinstance(value, int):
-            raise TypeError('data must be an integer')
-        self.__data = value
+    def data(self, data):
+        """Property setter to set data"""
+        if type(data) != int:
+            raise TypeError("data must be an integer")
+        self.__data = data
 
     @property
     def next_node(self):
-    """get next_node attribute
-    Returns: next node
-    """
-    return (self.__next_node)
+        """Property to retrieve next_node"""
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """set value of next node"""
-        if (value is not None and not isinstance(value, Node)):
-            raise TypeError('next_node must be a Node object')
+        """Property setter to set next_node"""
+        if type(value) != Node and value is not None:
+            raise TypeError("next_node must be a Node object")
         self.__next_node = value
-        class SinglyLinkedList:
-            """defines a singly linked list"""
+
+class SinglyLinkedList:
+    """Representation of a singly linked list"""
 
     def __init__(self):
-        """Initializes the singly linked list"""
-        self.head = None
-
-    def __str__(self):
-        """make list printable"""
-        printsll = ""
-        location = self.head
-        while location:
-            printsll += str(location.data) + "\n"
-            location = location.next_node
-            return printsll[:-1]
+        """Simple instantiation"""
+        self.__head = None
 
     def sorted_insert(self, value):
-    """insert in a sorted fashion
-    Args:
-    value: what the value will be on the node
-    """
-    new = Node(value)
-    if not self.head:
-        self.head = new
-        return
-    if value < self.head.data:
-        new.next_node = self.head
-        self.head = new
-        return
-    location = self.head
-    while location.next_node and location.next_node.data < value:
-        location = location.next_node
-        if location.next_node:
-            new.next_node = location.next_node
-            location.next_node = new
+        """Method that inserts a Node into the correct sorted position"""
+        if self.__head is None:
+            new_node = Node(value)
+            self.__head = new_node
+        elif self.__head.data > value:
+            new_node = Node(value, self.__head)
+            self.__head = new_node
+        else:
+            temp = self.__head
+            while temp.next_node is not None:
+                if temp.next_node.data > value:
+                    new_node = Node(value, temp.next_node)
+                    temp.next_node = new_node
+                    return
+                temp = temp.next_node
+            if temp.next_node is None:
+                new_node = Node(value)
+                temp.next_node = new_node
+
+    def __str__(self):
+        """Custom __str__ method to print the entire list in stdout"""
+        temp = self.__head
+        msg = ""
+        while temp is not None:
+            if temp.next_node is None:
+                msg += str(temp.data)
+            else:
+                msg += str(temp.data) + "\n"
+            temp = temp.next_node
+        return msg
