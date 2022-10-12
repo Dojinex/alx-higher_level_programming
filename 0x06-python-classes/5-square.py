7e4079b8f19c6f1dde3699d26e095e20d46e295a
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-12 01:17:21 -0700

    task 5 commit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,44 +1,36 @@
 #!/usr/bin/python3
-"""A module that defines a square """
+"""Square with size"""
+
+
 class Square:
-    """A class that represents a square"""
+    """Representation of a square"""
     def __init__(self, size=0):
-    """Initializing this square class
-    Args:
-        size: represnets the size of the square defined
-    Raises:
-        TypeError: if size is not integer
-        ValueError: if size is less than zero
-    """
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
-    if size < 0:
-        raise ValueError('size must be >= 0')
-    self.__size = size
+        """Instantiation with optional size"""
+        self.size = size
 
     @property
     def size(self):
-        """Retrieves size of square"""
-        return self.__size
+        """Property to retrieve size"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
+        """Property setter to set size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """
-        Calculate area of the square
-        Returns: The square of the size
-        """
-        return (self.__size ** 2)
+        """Public instance method that returns the current square area"""
+        return self.__size ** 2
 
     def my_print(self):
-        """print the square in # """
-        if self.__size == 0:
-            print()
+        """Public instance method that prints the square with char #"""
         for i in range(self.__size):
-            print("#" * self.__size)
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
