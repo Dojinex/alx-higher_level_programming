85b4d8129995d4314e24da1a94dd1f73377e349a
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-12 01:02:24 -0700

    task 4 commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """Square with size"""
+
+
 class Square:
     """Representation of a square"""
+
     def __init__(self, size=0):
         """Instantiation with optional size"""
         self.size = size
+
     @property
     def size(self):
         """Property to retrieve size"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         """Property setter to set size"""
@@ -17,6 +22,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """Public instance method that returns the current square area"""
         return self.__size ** 2
