0289caf4ea529008cf2e46c662b1844ac7d63b08
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-17 08:11:45 -0700

    Task 0 commit

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""A class that defines a rectangle"""
+""" Rectangle class """
+
+
 
 
 class Rectangle:
-    """An empty class that represents a rectangle"""
+    """ empty class Rectangle that defines a rectangle """
     pass
