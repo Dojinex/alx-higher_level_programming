939328f3bfe6422ef5eb2c8b456f81a50090ee02
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-24 10:41:13 -0700

    task 11 commit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-"""Square #2"""
+"""this module defines a Rectangle subclass Square"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """representation of a square"""
+    """Represent a square"""
 
     def __init__(self, size):
-        """instantiation of the square"""
+        """Initialize a new square"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        """custom str method for print the rectangle"""
-        return "[Square] " + str(self.__size) + "/" + str(self.__size)
-
-    def area(self):
-        """computes the area of the retangle"""
-        return self.__size ** 2
