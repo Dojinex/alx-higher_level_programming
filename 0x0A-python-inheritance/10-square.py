fb93a4346de28789db0a6fc7fd5af481656d73ad
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-24 10:34:14 -0700

    task 10 commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Square #1"""
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """representation of a square"""
+    """Represent a square"""
 
     def __init__(self, size):
-        """instantiation of the square"""
+        """Initialize a new square"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
