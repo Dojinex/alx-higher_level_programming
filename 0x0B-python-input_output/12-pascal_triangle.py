4791d8d80eeda72da110219ea084a5909cc48045
Dojinex <bitrusdauda9118@gmail.com.com>
2022-10-25 16:02:37 -0700

    task 12 commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Pascal's Triangle"""
 
-
 def pascal_triangle(n):
-    """returns a list of lists of integers
-    representing the Pascal’s triangle of n"""
+    """returns a list of lists of integers representing the Pascal’s triangle of n"""
+    
     triangle = []
     if n <= 0:
         return []
