f0ce06e5cdabc53e33ba92c8b317aa9e6d277250
Dojinex <bitrusdauda9118@gmail.com.com>
2022-12-25 05:37:57 -0800

    task 13 commit

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
-from model_state import Base, State
+"""script that prints the first State object from the database
+hbtn_0e_6_usa"""
 from sqlalchemy import (create_engine)
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
+import sys
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    """Connecting"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        session.delete(instance)
+    estados = session.query(State).filter(State.name.like('%a%'))
+    for sts in estados:
+        session.delete(sts)
     session.commit()
+    session.close()
